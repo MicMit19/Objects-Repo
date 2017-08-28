1d7f862687da65b6fe9410abebd36e397317b2f3
MicMit19 <30875208+MicMit19@users.noreply.github.com>
2017-08-28 08:49:10 -1000

    Update pres.js

--- pres.js
+++ pres.js
@@ -20,3 +20,21 @@ grade:"11",
 
 window.alert(students[x]);
 /* Creating a window alert that shows what the user asked for. But if the user did not enter an explicit command, this will not work. */
+
+/* AUgust 28, Replacing a new name */
+var newName = prompt("What is his new first name?")
+
+var Michael = {
+firstName:"Michael",
+lastName:"Mitamura",
+age:"16",
+grade:"11",
+}
+
+window.alert(Michael.firstName);
+window.alert(Michael.firstName = newName);
+
+/* Creating another window alert to see if this code is accurate, meaning we know we replaced Michael with a new name */
+
+
+
